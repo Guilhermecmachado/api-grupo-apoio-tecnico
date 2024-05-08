e35fe828bb1f7cf9334fdbc46aff4b7cd3406d67
Guilhermecmachado <guicamchado@gmail.com>
2024-05-08 13:04:07 -0300

    novos usuarios

--- src/controllers/usuarios-controller.js
+++ src/controllers/usuarios-controller.js
@@ -1,4 +1,4 @@
-const service = require('../services/projetos-service');
+const service = require('../services/usuarios-service');
 module.exports = {
 
 
@@ -8,17 +8,21 @@ module.exports = {
 
         let json = { error: '', result: {} };
         console.log('insert')
-        let entidade_nome = req.body.entidade_nome
-        let nome_projeto = req.body.nome_projeto
-        let codigo = req.body.codigo
-        let programa_nome = req.body.programa_nome
-        let status = req.body.status
+        let agencia = req.body.agencia
+        let nome = req.body.nome
+        let email = req.body.email
+        let banco = req.body.banco
+        let conta_corrente = req.body.conta_corrente
+        let cpf = req.body.cpf
+        let password = req.body.password
+        let perfil = req.body.perfil
+        let pix = req.body.pix
 
 
 
-        if (entidade_nome && nome_projeto && codigo && programa_nome && status) {
+        if (nome && email && cpf && password && perfil) {
             let model = await service.
-                inserir(entidade_nome, nome_projeto, codigo, programa_nome, status);
+                inserir(agencia, nome, email, banco, conta_corrente, cpf, password, perfil, pix);
             json.result = {
                 id: model,
             };
@@ -30,52 +34,55 @@ module.exports = {
 
     buscarTodos: async (req, res) => {
         let json = { error: '', result: [] };
-        let model = await service.buscarTodos();
+        let model = await service.buscarTodosUsuario();
         for (let i in model) {
             json.result.push({
                 id: model[i].id,
-                // nome_regional: model[i].nome_regional,
-                // status_regional: model[i].status_regional,
+                nome: model[i].nome,
+                email: model[i].email,
 
             });
         }
         res.json(json);
     },
 
-    // atualizar: async (req, res) => {
-    //     console.log('atualiza')
-    //     let json = { error: '', result: {} };
+    atualizar: async (req, res) => {
+        console.log('atualiza')
+        let json = { error: '', result: {} };
 
-    //     let id = req.params.id;
-    //     let nome_regional = req.body.nome_regional;
-    //     let status_regional = req.body.status_regional;
+        let id = req.params.id;
+        let agencia = req.body.agencia
+        let nome = req.body.nome
+        let email = req.body.email
+        let banco = req.body.banco
+        let conta_corrente = req.body.conta_corrente
+        let cpf = req.body.cpf
+        let password = req.body.password
+        let perfil = req.body.perfil
+        let pix = req.body.pix
 
-    //     let db_status = 0
-    //     if (status_regional == '0')
-    //         db_status = 0
-    //     else
-    //         db_status = 1
 
-    //     db_codigo = parseInt(id)
 
-    //     if (id && nome_regional && status_regional) {
-    //         await regionalInstalacaoService.atualizar(db_codigo, nome_regional, db_status);
-    //         json.result = {
-    //             id,
-    //             nome_regional,
 
-    //         };
-    //     } else {
-    //         json.error = 'Os campos não foram enviados';
-    //     }
-    //     res.json(json);
-    // },
+        db_codigo = parseInt(id)
+
+        if (id) {
+            await service.atualizar(db_codigo, agencia, nome, email, banco, conta_corrente, cpf, password, perfil, pix);
+            json.result = {
+                id,
+
+            };
+        } else {
+            json.error = 'Os campos não foram enviados';
+        }
+        res.json(json);
+    },
 
     buscarUm: async (req, res) => {
         let json = { error: '', result: {} };
         //  console.log('buscam um')
         let id = req.params.id; //para pegar o parametro
-        let result = await service.buscarUm(id);
+        let result = await service.buscarUmUsuario(id);
         res.json(result);
 
 
---
+++ src/services/usuarios-service.js
@@ -0,0 +1,307 @@
+
+const db = require('../../db');
+
+
+module.exports = {
+    inserirUsuario: (agencia, nome_projeto, email, banco, conta_corrente, cpf, password, perfil, pix) => {
+        return new Promise((accept, reject) => {
+
+            db.query('INSERT INTO tb_usuarios (agencia, nome_projeto, email, banco,  conta_corrente, cpf, password, perfil, pix) VALUES (?,?,?,?,?,?,?,?,?,?)',
+                [agencia, nome_projeto, email, banco, conta, conta_corrente, cpf, password, perfil, pix],
+                (error, results) => {
+                    if (error) { reject(error); return; }
+                    accept(results.insertId); //insertId
+                }
+            );
+        });
+    },
+    atualizar: (id, agencia, nome, email, banco, conta_corrente, cpf, password, perfil, pix) => {
+        return new Promise((aceito, rejeitado) => {
+            db.query('UPDATE gta_usuarios SET agencia=?, nome=?, email=?, banco=?, conta_corrente=?, cpf=?, password=?, perfil=?, pix=? WHERE id = ?',
+                [agencia, nome, email, banco, conta_corrente, cpf, password, perfil, pix, id],
+                (error, results) => {
+                    if (error) { rejeitado(error); return; }
+                    aceito(results);
+                }
+            );
+        });
+    },
+
+    inserir: (agencia, nome, email, banco, conta_corrente, cpf, password, perfil, pix) => {
+        return new Promise((accept, reject) => {
+
+            db.query('INSERT INTO gta_usuarios (agencia, nome, email, banco, conta_corrente, cpf, password, perfil, pix) VALUES (?,?,?,?,?,?,?,?,?)',
+                [agencia, nome, email, banco, conta_corrente, cpf, password, perfil, pix],
+                (error, results) => {
+                    if (error) { reject(error); return; }
+                    accept(results.insertId); //insertId
+                }
+            );
+        });
+    },
+
+    buscarTodosUsuario: () => {
+        return new Promise((aceito, rejeitado) => {
+            db.query('select * from gta_usuarios t order by t.nome', (error, results) => {
+                if (error) { rejeitado(error); return; }
+                aceito(results);
+            });
+        });
+    },
+
+    atualizarUsuario: (id, nome, cpf, senha, status_servico, email) => {
+        return new Promise((aceito, rejeitado) => {
+            db.query('UPDATE tb_usuarios SET nome = ?, cpf = ? , senha = ? ,status = ?, email =?  WHERE id = ?',
+                [nome, cpf, senha, status_servico, email, id],
+                (error, results) => {
+                    if (error) { rejeitado(error); return; }
+                    aceito(results);
+                }
+            );
+        });
+    },
+    buscarUmUsuario: (id) => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('SELECT * FROM gta_usuarios WHERE id = ?', [id], (error, results) => {
+                if (error) { rejeitado(error); return; }
+                if (results.length > 0) {
+                    aceito(results[0]);
+                } else {
+                    aceito(false);
+                }
+            });
+        });
+    },
+
+    buscarUsuariosPermissoes: () => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('SELECT * FROM tb_usuarios_permissao', (error, results) => {
+                if (error) { rejeitado(error); return; }
+                if (results.length > 0) {
+                    aceito(results);
+                } else {
+                    aceito(false);
+                }
+            });
+        });
+    },
+
+
+    verificaSeExiste: (email, senha) => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('SELECT * FROM tb_usuarios WHERE email = ? AND senha =?', [email, senha], (error, results) => {
+                if (error) { rejeitado(error); return; }
+                if (results.length > 0) {
+                    aceito(results[0]);
+                } else {
+                    aceito(false);
+                }
+            });
+        });
+    },
+
+    verificaSeExisteApp: (email, senha) => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('SELECT * FROM tb_tecnicos WHERE email = ? AND senha =?', [email, senha], (error, results) => {
+                if (error) { rejeitado(error); return; }
+                if (results.length > 0) {
+                    aceito(results[0]);
+                } else {
+                    aceito(false);
+                }
+            });
+        });
+    },
+
+    inserirUsuarioPermissao: (rota_id, permissao, usuario_id, nome_rota) => {
+        return new Promise((accept, reject) => {
+
+            db.query('INSERT INTO tb_usuarios_permissao (rota_id,permissao,usuario_id,nome_rota) VALUES (?,?,?,?)',
+                [rota_id, permissao, usuario_id, nome_rota],
+                (error, results) => {
+                    if (error) { reject(error); return; }
+                    accept(results.insertId); //insertId
+                }
+            );
+        });
+    },
+
+    inserirUsuarioPermissaoCriar: (rota_id, criar, usuario_id, nome_rota) => {
+        return new Promise((accept, reject) => {
+
+            db.query('INSERT INTO tb_usuarios_permissao (rota_id,criar,usuario_id,nome_rota) VALUES (?,?,?,?)',
+                [rota_id, criar, usuario_id, nome_rota],
+                (error, results) => {
+                    if (error) { reject(error); return; }
+                    accept(results.insertId); //insertId
+                }
+            );
+        });
+    },
+
+    inserirUsuarioPermissaoEditar: (rota_id, editar, usuario_id, nome_rota) => {
+        return new Promise((accept, reject) => {
+
+            db.query('INSERT INTO tb_usuarios_permissao (rota_id,editar,usuario_id,nome_rota) VALUES (?,?,?,?)',
+                [rota_id, editar, usuario_id, nome_rota],
+                (error, results) => {
+                    if (error) { reject(error); return; }
+                    accept(results.insertId); //insertId
+                }
+            );
+        });
+    },
+
+    buscarTodosRotas: () => {
+        return new Promise((aceito, rejeitado) => {
+            db.query('select * from tb_itens_menu t order by t.nome_rota', (error, results) => {
+                if (error) { rejeitado(error); return; }
+                aceito(results);
+            });
+        });
+    },
+
+    buscarUmUsuarioPermissao: (id_usuario, id_rota) => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('SELECT * FROM tb_usuarios_permissao WHERE usuario_id = ? AND rota_id =?', [id_usuario, id_rota], (error, results) => {
+                if (error) { rejeitado(error); return; }
+                if (results.length > 0) {
+                    aceito(results);
+                } else {
+                    aceito(false);
+                }
+            });
+        });
+    },
+
+    buscarUmUsuarioPermissaoRota: (id_usuario, id_rota) => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('SELECT * FROM tb_usuarios_permissao WHERE usuario_id = ? AND rota_id =? AND permissao =1', [id_usuario, id_rota], (error, results) => {
+                if (error) { rejeitado(error); return; }
+                if (results.length > 0) {
+                    aceito(results);
+                } else {
+                    aceito(false);
+                }
+            });
+        });
+    },
+
+    buscarUmUsuarioPermissaoUsuario: (id_usuario) => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('SELECT * FROM `tb_usuarios_permissao` WHERE usuario_id = ? AND rota_id = 11 AND criar =1 AND editar = 1', [id_usuario], (error, results) => {
+                if (error) { rejeitado(error); return; }
+                if (results.length > 0) {
+                    aceito(results);
+                } else {
+                    aceito(false);
+                }
+            });
+        });
+    },
+
+
+
+    buscarUmUsuarioPermissaoCria: (id_usuario, id_rota) => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('SELECT * FROM tb_usuarios_permissao WHERE usuario_id = ? AND rota_id =? AND criar = 1', [id_usuario, id_rota], (error, results) => {
+                if (error) { rejeitado(error); return; }
+                if (results.length > 0) {
+                    aceito(results);
+                } else {
+                    aceito(false);
+                }
+            });
+        });
+    },
+
+    verificaUsuarioPermissaoCria: (id_usuario, id_rota) => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('SELECT * FROM tb_usuarios_permissao WHERE usuario_id = ? AND rota_id =?', [id_usuario, id_rota], (error, results) => {
+                if (error) { rejeitado(error); return; }
+                if (results.length > 0) {
+                    aceito(results);
+                } else {
+                    aceito(false);
+                }
+            });
+        });
+    },
+
+    verificaUsuarioPermissaoEdita: (id_usuario, id_rota) => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('SELECT * FROM tb_usuarios_permissao WHERE usuario_id = ? AND rota_id =?', [id_usuario, id_rota], (error, results) => {
+                if (error) { rejeitado(error); return; }
+                if (results.length > 0) {
+                    aceito(results);
+                } else {
+                    aceito(false);
+                }
+            });
+        });
+    },
+
+    buscarUmUsuarioPermissaoEdita: (id_usuario, id_rota) => {
+        return new Promise((aceito, rejeitado) => {
+
+            db.query('SELECT * FROM tb_usuarios_permissao WHERE usuario_id = ? AND rota_id =? AND editar = 1', [id_usuario, id_rota], (error, results) => {
+                if (error) { rejeitado(error); return; }
+                if (results.length > 0) {
+                    aceito(results);
+                } else {
+                    aceito(false);
+                }
+            });
+        });
+    },
+
+
+
+
+    atualizarUsuarioPermissao: (id, permissao) => {
+        return new Promise((aceito, rejeitado) => {
+            db.query('UPDATE tb_usuarios_permissao SET permissao = ? WHERE id = ?',
+                [permissao, id],
+                (error, results) => {
+                    if (error) { rejeitado(error); return; }
+                    aceito(results);
+                }
+            );
+        });
+    },
+
+    atualizarUsuarioPermissaoCriar: (id, criar) => {
+        return new Promise((aceito, rejeitado) => {
+            db.query('UPDATE tb_usuarios_permissao SET criar = ? WHERE id = ?',
+                [criar, id],
+                (error, results) => {
+                    if (error) { rejeitado(error); return; }
+                    aceito(results);
+                }
+            );
+        });
+    },
+
+
+    atualizarUsuarioPermissaoEditar: (id, editar) => {
+        return new Promise((aceito, rejeitado) => {
+            db.query('UPDATE tb_usuarios_permissao SET editar = ? WHERE id = ?',
+                [editar, id],
+                (error, results) => {
+                    if (error) { rejeitado(error); return; }
+                    aceito(results);
+                }
+            );
+        });
+    },
+}
