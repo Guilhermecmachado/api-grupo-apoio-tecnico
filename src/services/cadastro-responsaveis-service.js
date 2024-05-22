973d694d249adf038641c8f8dd8d5d9e57d59f7c
Guilhermecmachado <guicamchado@gmail.com>
2024-05-22 14:24:23 -0300

    .

--- src/services/cadastro-responsaveis-service.js
+++ src/services/cadastro-responsaveis-service.js
@@ -18,7 +18,7 @@ module.exports = {
 
     buscarTodos: () => {
         return new Promise((aceito, rejeitado) => {
-            db.query('select * from gta_cadastro_responsaveis  t order by t.cadastro_nome', (error, results) => {
+            db.query('select * from gta_cadastro_responsaveis', (error, results) => {
                 if (error) { rejeitado(error); return; }
                 aceito(results);
             });
