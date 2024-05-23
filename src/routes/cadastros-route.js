b15a491998d8c3583032a701007bed99f53d29e1
Guilhermecmachado <guicamchado@gmail.com>
2024-05-22 22:51:02 -0300

    .

--- src/routes/cadastros-route.js
+++ src/routes/cadastros-route.js
@@ -12,8 +12,10 @@ router.get('/', controller.buscarTodos)
 router.get('/:id', controller.buscarUm)
 router.get('/numero_cadastro/:numero_cadastro', controller.buscarUmApp)
 router.get('/form/:id', controller.buscarUmForm)
-router.get('/:id/:numero_cadastro', controller.buscarCadastros)
-router.get('/cadastros_dados/:numero_cadastro/:nome_tabela', controller.buscarCadastrosResponsavel)
+router.get('/:id/:numero_cadastro', controller.buscarUmCadastro)
+router.get('/cadastros_dados/:numero_cadastro/:nome_tabela', controller.buscarCadastros)
+router.get('/cadastros_dados/:numero_cadastro/:nome_tabela/:tipo_cadastro', controller.buscarCadastrosResponsavel)
+
 router.post('/:id', controller.atualizar)
 
 
