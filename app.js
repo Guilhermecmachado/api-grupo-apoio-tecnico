b84456a701c1f682c441d26463ae4881bb703cf1
Guilhermecmachado <guicamchado@gmail.com>
2024-07-12 15:23:56 -0300

    .

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const debug = require('debug')('apiwa:server')
 const app = express()
 const jwt = require('jsonwebtoken')
-const port = normalizePort(process.env.PORT || '8002')
+const port = normalizePort(process.env.PORT || '8003')
 app.set('port', port)
 
 //server
