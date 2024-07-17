5c857d483d46fb34b1bc4bdc077985851822a709
Guilhermecmachado <guicamchado@gmail.com>
2024-07-17 13:21:29 -0300

    .

--- src/routes/cadastro-demografico-route.js
+++ src/routes/cadastro-demografico-route.js
@@ -11,7 +11,7 @@ router.post('/', controller.inserir)
 router.get('/', controller.buscarTodos)
 router.get('/:id/:numero_cadastro', controller.buscarUm)
 router.get('/:id/form_dados', controller.buscarUmForm)
-router.get9('/:uuid/uuid', controller.buscarUmUiid)
+router.get('/:uuid', controller.buscarUmUiid)
 router.post('/:id', controller.atualizar)
 
 
